d513c5ce82f76c9cb3521c5faeb54d93ca6bf8d6
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2024-05-26 11:08:25 -0500

    feat: add curtain animation and roulette

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class FrontPageComponent {
 
+  curtainsOpened = false;
+
+  OpenCurtains() {
+    const curtainLeft = document.getElementById("curtainLeft")!;
+    const curtainRight = document.getElementById("curtainRight")!;
+    curtainLeft.style.transition = "all 1s ease-out"
+    curtainRight.style.transition = "all 1s ease-out"
+
+    curtainLeft.style.transform = "translateX(-100%)";
+    curtainRight.style.transform = "translateX(100%)";
+
+    setTimeout(() => {
+      this.curtainsOpened = true
+    }, 1000);
+  }
+
+  spinRoulette() {
+    const roulette = document.getElementById('roulette')!;
+    let rand = Math.random() * 7200;
+    roulette.style.transform = "rotate("+rand+"deg)";
+    roulette.style.transition = "all 5s";
+    setTimeout(() => {
+      console.log("ganaste")
+    }, 5000);
+  }
 }
